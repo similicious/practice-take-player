0a66e22843f82cafdaeb1789fec893b592c356d8
similicious <mail@similicious.com>
2023-01-26 19:43:08 +0100

    Revert slider to remove mute bug

--- src/VolumeControls.svelte
+++ src/VolumeControls.svelte
@@ -4,11 +4,22 @@
   export let volume: Pick<Channel, "volume">;
 </script>
 
+<!-- 
 <Slider
   bind:value={volume.volume.value}
   max={0}
   min={-42}
   step={1}
+  disabled={volume.volume.value === -Infinity}
+  style="flex: 1"
+/> -->
+
+<input
+  type="range"
+  bind:value={volume.volume.value}
+  max="0"
+  min="-42"
+  step="1"
   disabled={volume.volume.value === -Infinity}
   style="flex: 1"
 />
